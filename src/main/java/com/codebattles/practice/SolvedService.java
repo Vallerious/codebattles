f8d6cf13975da4bd0306b41fb591e1cf75456dfe
Valeri Valeriev <valericfbg@gmail.com>
2018-08-24 00:29:52 +0300

    connecting charts to backend

--- src/main/java/com/codebattles/practice/SolvedService.java
+++ src/main/java/com/codebattles/practice/SolvedService.java
@@ -1,5 +1,8 @@
 package com.codebattles.practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.management.Query;
 import javax.websocket.Session;
 
@@ -8,14 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.codebattles.user.CodebattlesUser;
+import com.mysql.fabric.xmlrpc.base.Array;
 
 @Service
 public class SolvedService implements ISolvedService {
   private SolvedRepository solvedRepository;
   
+  private PracticeProblemRepository practiceProblemRepository;
+  
   @Autowired
-  public SolvedService(SolvedRepository solvedRepository) {
+  public SolvedService(SolvedRepository solvedRepository, PracticeProblemRepository practiceProblemRepository) {
     this.solvedRepository = solvedRepository;
+    this.practiceProblemRepository = practiceProblemRepository;
   }
   
   public Solved recordSolvedProblem(CodebattlesUser user, PracticeProblem practiceProblem) {
@@ -32,4 +39,20 @@ public class SolvedService implements ISolvedService {
     
     return problemId != null;
   }
+
+  @Override
+  public List<Long[]> getDateRating(String userId) {
+    List<Solved> solveds = this.solvedRepository.findByUserId(userId);
+    List<Long[]> dateRating = new ArrayList<>();
+    
+    for (Solved solv : solveds) {
+      PracticeProblem practiceProblem = solv.getProblem();
+      Long[] dr = {(long) solv.getDateSolved().getTime(), practiceProblem.getPoints()};
+      
+      dateRating.add(dr);
+    }
+    
+    return dateRating;
+  }
+
 }
